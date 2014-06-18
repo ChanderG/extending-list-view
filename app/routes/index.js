f4ae012302ad7c582ae98f0b1551904b149dc7b9
ChanderG <chandergovind@gmail.com>
2014-06-18 13:26:59 +0530

    Got basic functionality;using table for each row

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ export default Ember.Route.extend({
   model: function() {
     var items = [];
     for(var i=0; i< 100; i++){
-      items.push({name: "" + i});
+      items.push({id  :  i,
+                  name: "Lorem"});
     }
     return items;
   }
